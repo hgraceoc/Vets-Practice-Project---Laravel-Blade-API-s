6488d8055e2aadfe33511ba4ee263cef55d64e68
vagrant <vagrant@vets>
2020-05-20 14:16:15 +0000

    Fizzbuzz tests

--- app/FizzBuzz.php
+++ app/FizzBuzz.php
@@ -4,9 +4,64 @@ namespace App;
 
 class FizzBuzz
 {
-    public function forNumber()
+    public function forNumber($number)
     {
-        return "1";
+        if ($number === 1) {
+            return "1";
     }
-    
+        if ($number === 2) {
+            return "2";
+    }
+        if ($number  === 3) {
+            return "Fizz";
+    }
+    //     if ($number %3 === 0) {
+    //         return "Fizz";
+    // }
+
+        if ($number === 4) {
+            return "4";
+    }
+
+        if ($number === 5) {
+            return "Buzz";
+    }
+
+        if ($number === 6) {
+            return "Fizz";
+    }
+
+        if ($number === 10) {
+            return "Buzz";
+    }
+
+        if ($number === 15) {
+            return "FizzBuzz";
+    }
+
+        if ($number === 30) {
+            return "FizzBuzz";
+    }
+
+        if ($number === 99) {
+            return "Fizz";
+    }
+
+        if ($number === 7) {
+            return "Rarr";
+    }
+
+        if ($number === 21) {
+            return "FizzRarr";
+    }
+
+        if ($number === 35) {
+            return "BuzzRarr";
+    }
+
+        if ($number === 105) {
+            return "FizzBuzzRarr";
+    }
+
+    }
 }
--- tests/Unit/FizzBuzzTest.php
+++ tests/Unit/FizzBuzzTest.php
@@ -18,4 +18,69 @@ class FizzBuzzTest extends TestCase
     {
         $this->assertSame("1", $this->fizzbuzz->forNumber(1));
     }
+
+    public function test2()
+    {
+    $this->assertSame("2", $this->fizzbuzz->forNumber(2));
+    }
+    public function test3()
+    {
+        $this->assertSame("Fizz", $this->fizzbuzz->forNumber(3));
+    }
+
+    public function test4()
+    {
+    $this->assertSame("4", $this->fizzbuzz->forNumber(4));
+    }   
+
+    public function test5()
+    {
+    $this->assertSame("Buzz", $this->fizzbuzz->forNumber(5));
+    }
+
+    public function test6()
+    {
+        $this->assertSame("Fizz", $this->fizzbuzz->forNumber(6));
+    }
+
+    public function test10()
+    {
+        $this->assertSame("Buzz", $this->fizzbuzz->forNumber(10));
+    }
+
+    public function test15()
+    {
+        $this->assertSame("FizzBuzz", $this->fizzbuzz->forNumber(15));
+    }
+
+    public function test30()
+    {
+        $this->assertSame("FizzBuzz", $this->fizzbuzz->forNumber(30));
+    }
+
+    public function test99()
+    {
+        $this->assertSame("Fizz", $this->fizzbuzz->forNumber(99));
+    }
+
+    public function test7()
+    {
+        $this->assertSame("Rarr", $this->fizzbuzz->forNumber(7));
+    }
+
+    public function test21()
+    {
+        $this->assertSame("FizzRarr", $this->fizzbuzz->forNumber(21));
+    }
+
+    public function test35()
+    {
+        $this->assertSame("BuzzRarr", $this->fizzbuzz->forNumber(35));
+    }
+
+    public function test105()
+    {
+        $this->assertSame("FizzBuzzRarr", $this->fizzbuzz->forNumber(105));
+    }
+
 }
