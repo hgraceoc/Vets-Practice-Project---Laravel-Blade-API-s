c4ac785721ef9e6643e0a7eac3599de5180d4568
Hannah Cryer <hannahcryer@hotmail.com>
2020-05-21 14:42:51 +0100

    updates to animal test

--- app/Alphabet.php
+++ app/Alphabet.php
@@ -7,67 +7,13 @@ class Alphabet
 
     public function forLetter(string $letter)
     {
-        if ($letter === "a") {
-            return "!";
-
-    }   if ($letter === "b") {
-        return ")";
-
-    }   if ($letter === "c") {
-        return "#";
-
-    }   if ($letter === "d") {
-        return "(";
-
-    }   if ($letter === "e") {
-        return "£";
-    
-    }  if ($letter === "f") {
-        return "*";
-    
-    }  if ($letter === "g") {
-        return "%";
-    
-    }  if ($letter === "h") {
-        return "&";
-    
-    } if ($letter === "i") {
-        return ">";
-    
-    } if ($letter === "j") {
-        return "<";
-    
-    } if ($letter === "k") {
-        return "@";
-    
-    } if ($letter === "l") {
-        return "a";
-    
-    } if ($letter === "m") {
-        return "b";
-    
-    } if ($letter === "n") {
-        return "c";
-    
-    } if ($letter === "o") {
-        return "d";
-    
-    } if ($letter === "p") {
-        return "e";
-    
-    } if ($letter === "q") {
-        return "f";
-    
-    } if ($letter === "r") {
-        return "g";
-    
-    } if ($letter === "s") {
-        return "h";
-    
-    } if ($letter === "t") {
-        return "i";
+        if ($letter === "!") {
+            return "a";
     }
 
- 
+    public function 
 }
+
+
+
 }
--- tests/Unit/AlphabetTest.php
+++ tests/Unit/AlphabetTest.php
@@ -9,117 +9,146 @@ class AlphabetTest extends TestCase
 {
     private $letter;
 
+
     public function setUp() : void 
     {
         parent::setUp();
         $this->letter = new Alphabet();
     }
 
-    public function test_a()
+    public function test_bang()
     {
-        $this->assertSame("!", $this->letter->forLetter("a"));
+        $this->assertSame("a", $this->letter->forLetter("!"));
     }
 
-    public function test_b()
-    {
-        $this->assertSame(")", $this->letter->forLetter("b"));
-    }
+    // public function test_b()
+    // {
+    //     $this->assertSame(")", $this->letter->forLetter("b"));
+    // }
 
-    public function test_c()
-    {
-        $this->assertSame("#", $this->letter->forLetter("c"));
-    }
+    // public function test_c()
+    // {
+    //     $this->assertSame("#", $this->letter->forLetter("c"));
+    // }
 
-    public function test_d()
-    {
-        $this->assertSame("(", $this->letter->forLetter("d"));
-    }
+    // public function test_d()
+    // {
+    //     $this->assertSame("(", $this->letter->forLetter("d"));
+    // }
 
-    public function test_e()
-    {
-        $this->assertSame("£", $this->letter->forLetter("e"));
-    }
+    // public function test_e()
+    // {
+    //     $this->assertSame("£", $this->letter->forLetter("e"));
+    // }
 
-    public function test_f()
-    {
-        $this->assertSame("*", $this->letter->forLetter("f"));
-    }
+    // public function test_f()
+    // {
+    //     $this->assertSame("*", $this->letter->forLetter("f"));
+    // }
 
-    public function test_g()
-    {
-        $this->assertSame("%", $this->letter->forLetter("g"));
-    }
+    // public function test_g()
+    // {
+    //     $this->assertSame("%", $this->letter->forLetter("g"));
+    // }
 
-    public function test_h()
-    {
-        $this->assertSame("&", $this->letter->forLetter("h"));
-    }
+    // public function test_h()
+    // {
+    //     $this->assertSame("&", $this->letter->forLetter("h"));
+    // }
 
-    public function test_i()
-    {
-        $this->assertSame(">", $this->letter->forLetter("i"));
-    }
+    // public function test_i()
+    // {
+    //     $this->assertSame(">", $this->letter->forLetter("i"));
+    // }
 
-    public function test_j()
-    {
-        $this->assertSame("<", $this->letter->forLetter("j"));
-    }
+    // public function test_j()
+    // {
+    //     $this->assertSame("<", $this->letter->forLetter("j"));
+    // }
 
-    public function test_k()
-    {
-        $this->assertSame("@", $this->letter->forLetter("k"));
-    }
+    // public function test_k()
+    // {
+    //     $this->assertSame("@", $this->letter->forLetter("k"));
+    // }
 
-    public function test_l()
-    {
-        $this->assertSame("a", $this->letter->forLetter("l"));
-    }
+    // public function test_l()
+    // {
+    //     $this->assertSame("a", $this->letter->forLetter("l"));
+    // }
 
-    public function test_m()
-    {
-        $this->assertSame("b", $this->letter->forLetter("m"));
-    }
+    // public function test_m()
+    // {
+    //     $this->assertSame("b", $this->letter->forLetter("m"));
+    // }
 
-    public function test_n()
-    {
-        $this->assertSame("c", $this->letter->forLetter("n"));
-    }
+    // public function test_n()
+    // {
+    //     $this->assertSame("c", $this->letter->forLetter("n"));
+    // }
 
-    public function test_o()
-    {
-        $this->assertSame("d", $this->letter->forLetter("o"));
-    }
+    // public function test_o()
+    // {
+    //     $this->assertSame("d", $this->letter->forLetter("o"));
+    // }
 
-    public function test_p()
-    {
-        $this->assertSame("e", $this->letter->forLetter("p"));
-    }
+    // public function test_p()
+    // {
+    //     $this->assertSame("e", $this->letter->forLetter("p"));
+    // }
 
-    public function test_q()
-    {
-        $this->assertSame("f", $this->letter->forLetter("q"));
-    }
+    // public function test_q()
+    // {
+    //     $this->assertSame("f", $this->letter->forLetter("q"));
+    // }
 
-    public function test_r()
-    {
-        $this->assertSame("g", $this->letter->forLetter("r"));
-    }
+    // public function test_r()
+    // {
+    //     $this->assertSame("g", $this->letter->forLetter("r"));
+    // }
 
-    public function test_s()
-    {
-        $this->assertSame("h", $this->letter->forLetter("s"));
-    }
+    // public function test_s()
+    // {
+    //     $this->assertSame("h", $this->letter->forLetter("s"));
+    // }
 
-    public function test_t()
-    {
-        $this->assertSame("i", $this->letter->forLetter("t"));
-    }
+    // public function test_t()
+    // {
+    //     $this->assertSame("i", $this->letter->forLetter("t"));
+    // }
+
+    // public function test_u()
+    // {
+    //     $this->assertSame("j", $this->letter->forLetter("u"));
+    // }
+
+    // public function test_v()
+    // {
+    //     $this->assertSame("k", $this->letter->forLetter("v"));
+    // }
+
+    // public function test_w()
+    // {
+    //     $this->assertSame("l", $this->letter->forLetter("w"));
+    // }
+
+    // public function test_x()
+    // {
+    //     $this->assertSame("m", $this->letter->forLetter("x"));
+    // }
+
+    // public function test_y()
+    // {
+    //     $this->assertSame("n", $this->letter->forLetter("y"));
+    // }
+
+    // public function test_z()
+    // {
+    //     $this->assertSame("o", $this->letter->forLetter("z"));
+    // }
+
+
 
 
 }
 
-// public function testFull()
-// {
-//     $cracker = new Cracker("! ) # ( £ * % & > < @ a b c d e f g h i j k l m n o");
-//     $this->assertSame("hello mum", $cracker->decrypt("&£aad bjb"));
-// }
+
